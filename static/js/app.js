fb5ba6f7f6bd889e543b4af6ba47bdf11a28bf52
shams <shams@viratech.ir>
2017-03-31 20:16:42 +0430

    pagination logic changed

--- static/js/app.js
+++ static/js/app.js
@@ -6,17 +6,6 @@ function main_controller($scope,$http) {
 
     init($http,$scope);
 
-    $scope.page_count = 0;
-    $scope.current_page= 0;
-
-    $scope.getNumberOfPages = function () {
-        if($scope.page_count == null)
-        {
-            $scope.page_count
-        }
-        return new Array($scope.page_count);
-    };
-
     $scope.change_page = function (page_number) {
         $scope.current_page = page_number;
         $scope.data = [];
@@ -40,14 +29,18 @@ function main_controller($scope,$http) {
 
         change_style(context.sidebar_selector_id,context.navbar_selector_id);
     };
-
-    $scope.page_numbering = "templates/page-numbering.html";
-
 }
-var page_capacity = 6;
+var page_capacity;
 var dict;
 var context;
 function init($http,$scope) {
+
+    $scope.page_count = 0;
+    $scope.current_page= 0;
+    page_capacity = 6;
+
+    $scope.pagination = "templates/pagination.html";
+
     $http.get("test_data/user.json").then(function (response) {
         $scope.user = response.data;
     });
