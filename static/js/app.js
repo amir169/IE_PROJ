ae35ccf1e87c68cdf66bc22739b2180159b5aaf0
shams <shams@viratech.ir>
2017-04-01 17:41:51 +0430

    pagination appearance changed and some refactoring

--- static/js/app.js
+++ static/js/app.js
@@ -13,30 +13,27 @@ function main_controller($scope,$http) {
 
         $scope.current_page = page_number;
         $scope.data = [];
-        $http.get(context.data_url).then( function(response) {
+        $http.get($scope.context.data_url).then( function(response) {
             $scope.data = response.data.slice(($scope.current_page - 1) * page_capacity, $scope.current_page * page_capacity);
         });
     };
     $scope.something = "test_data/announcement_post.html";
     $scope.change_context = function (context_name) {
-        context = dict[context_name];
+        $scope.context = dict[context_name];
         $scope.data = [];
         $scope.record = {};
-        $scope.template = context.template_url;
+        $scope.template = $scope.context.template_url;
         $scope.current_page = 1;
-        $http.get(context.data_url).then( function(response) {
+        $http.get($scope.context.data_url).then( function(response) {
 
             // console.log(response.data);
             $scope.data = response.data.slice(($scope.current_page - 1) * page_capacity, $scope.current_page * page_capacity);
             $scope.page_count = Math.ceil(response.data.length / page_capacity);
         });
-
-        change_style(context.sidebar_selector_id,context.navbar_selector_id);
     };
 }
 var page_capacity;
 var dict;
-var context;
 function init($http,$scope) {
 
     $scope.page_count = 0;
@@ -53,8 +50,4 @@ function init($http,$scope) {
         dict = response.data;
         $scope.change_context('games');
     });
-}
-function change_style(sidebar,navbar) {
-    $("#" + sidebar).addClass("sidebar-selected").siblings().removeClass("sidebar-selected");
-    $("#" + navbar).addClass("active").siblings().removeClass("active");
 }
