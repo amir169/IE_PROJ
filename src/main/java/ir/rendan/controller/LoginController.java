9ee3a5a4e434090302fc348fe4c197d6523b1ee9
shams <shams@viratech.ir>
2017-04-30 15:35:29 +0430

    a little change

--- src/main/java/ir/rendan/controller/LoginController.java
+++ src/main/java/ir/rendan/controller/LoginController.java
@@ -31,6 +31,7 @@ public class LoginController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        response.getWriter().write("hi");
     }
 
     /**
