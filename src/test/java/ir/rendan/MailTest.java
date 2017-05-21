8fd76edd9ad9ebff0fa3cd40aec3c9f3d09eaaff
shams <shams@viratech.ir>
2017-05-21 11:56:09 +0430

    email sender code added

---
+++ src/test/java/ir/rendan/MailTest.java
@@ -0,0 +1,47 @@
+package ir.rendan;
+
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+class Mailer{
+    public static void send(String from,String password,String to,String sub,String msg){
+        //Get properties object
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+
+
+        //get Session
+        Session session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(from,password);
+                    }
+                });
+        //compose message
+        try {
+
+            MimeMessage message = new MimeMessage(session);
+            message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));
+            message.setSubject(sub);
+            message.setText(msg);
+            message.setFrom(new InternetAddress(from));
+            //send message
+            Transport.send(message);
+            System.out.println("message sent successfully");
+        } catch (MessagingException e) {throw new RuntimeException(e);}
+
+    }
+}
+public class MailTest {
+    public static void main(String[] args) {
+        //from,password,to,subject,message
+        Mailer.send("username@gmail.com","password"
+                ,"to@example.com","test","this is a test");
+        //change from, password and to
+    }
+}    
