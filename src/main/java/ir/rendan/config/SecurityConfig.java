6513c27df24a3d60d3c45a2722f604492ea35ed3
shams <shams@viratech.ir>
2017-06-12 01:55:34 +0430

    question delete and some refactoring

--- src/main/java/ir/rendan/config/SecurityConfig.java
+++ src/main/java/ir/rendan/config/SecurityConfig.java
@@ -1,10 +1,8 @@
 
 package ir.rendan.config;
 
-import ir.rendan.services.MyAppUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,13 +12,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  // */
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final MyAppUserDetailsService myAppUserDetailsService;
-
-    public SecurityConfig(MyAppUserDetailsService myAppUserDetailsService) {
-        this.myAppUserDetailsService = myAppUserDetailsService;
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -45,11 +38,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/question/reply"
                         ,"/games/**/admin.html").hasAuthority("ADMIN")
                 .anyRequest().authenticated();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(myAppUserDetailsService);
     }
 
 }
