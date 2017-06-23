94dbf6b4a92503a89e2a62fd653ad64a69d298b8
shams <shams@viratech.ir>
2017-06-23 17:25:52 +0430

    game repository fixed

--- src/main/java/ir/rendan/repository/GameRepository.java
+++ src/main/java/ir/rendan/repository/GameRepository.java
@@ -1,10 +1,11 @@
 package ir.rendan.repository;
 
+import ir.rendan.model.Game;
 import ir.rendan.repository.custom.PagingAndSortingRepositoryWithReadOnlyRestResource;
 
 /**
  * Created by Amir Shams on 6/23/2017.
  */
-public interface GameRepository extends PagingAndSortingRepositoryWithReadOnlyRestResource {
+public interface GameRepository extends PagingAndSortingRepositoryWithReadOnlyRestResource<Game,Integer> {
 
 }
